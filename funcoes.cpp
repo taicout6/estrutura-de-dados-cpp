d56c6cda1b4ad2dea1160f7dcf6fb1a3489e2d82
Tainá Coutinho <taicout6@gmail.com>
2024-07-29 15:56:31 -0300

    Declaração de Funções

---
+++ funcoes.cpp
@@ -0,0 +1,15 @@
+int sum(int n1, int n2) {
+    return n1 + n2;
+}
+
+int sub(int n1, int n2) {
+    return n1 - n2;
+}
+
+int mul(int n1, int n2) {
+    return n1 * n2;
+}
+
+int idiv(int n1, int n2) {
+    return n1 / n2;
+}
