cea2f6f96860d2ed926f802d23faaf628a8c75a5
Tainá Coutinho <taicout6@gmail.com>
2024-08-06 16:07:26 -0300

    Classes

---
+++ time.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "time.h"
+
+using namespace std;
+
+int Time::getHour() const {
+    return hour;
+}
+
+int  Time::getMinute() const {
+    return minute;
+}
+
+int Time::getSecond() const {
+    return second;
+}
+
+void Time::setHour(int hour) {
+    this->hour = hour;
+}
+
+void Time::setMinute(int minute) {
+    this->minute = minute;
+}
+
+void Time::setSecond(int second) {
+    this->second = second;
+}
+
+void Time::print() const {
+    cout << hour << ":" << minute << ":" << second << endl;
+}
+
+void Time::nextSecond() {
+    second += 1;
+    if (second >= 60) {
+        second = 0;
+        minute += 1;
+    }
+    if (minute >= 60) {
+        minute = 0;
+        hour += 1;
+    }
+    if (hour >= 24) {
+        hour = 0;
+    }
+}
+
+Time t1(23, 59, 59);
---
+++ time.h
@@ -0,0 +1,19 @@
+#ifndef TIME_H
+#define TIME_H
+
+class Time {
+    private:
+        int hour;
+        int minute;
+        int second;
+    public:
+        Time(int hour = 0, int minute = 0, int second = 0);
+        int getHour() const;
+        void setHour(int hour);
+        int getMinute() const;
+        void setMinute(int minute);
+        int getSecond() const;
+        void setSecond(int second);
+        void print() const;
+        void nextSecond();
+};
