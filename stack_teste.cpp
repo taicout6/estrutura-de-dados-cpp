d8fe758a66b51124e0f721cdf1bb4da6ebd26ecf
Tainá Coutinho <taicout6@gmail.com>
2024-08-07 08:32:47 -0300

    Pilha(vetor)

---
+++ stack_teste.cpp
@@ -0,0 +1,27 @@
+#include "stack.h"
+#include <iostream>
+#include <ostream>
+
+using namespace std;
+
+int main() {
+    ItemType character;
+    Stack stack;
+    ItemType stackItem;
+
+    cout << "Adicione uma String" << endl;
+    cin.get(character);
+
+    while (character != '\n') {
+        stack.push(character);
+        cin.get(character);
+    }
+
+    while (!stack.isEmpty()) {
+        stackItem = stack.pop();
+        cout << stackItem;
+    }
+
+    cout << endl;
+
+}
---
+++ stack_vetor.cpp
@@ -0,0 +1,48 @@
+#include "stack.h"
+#include <iostream>
+
+using namespace std;
+
+Stack::Stack() {
+    length = 0;
+    structure = new ItemType[MAX_ITEMS];
+}
+
+Stack::~Stack() {
+    delete [] structure;
+}
+
+bool Stack::isEmpty() const {
+    return (length == 0);
+}
+
+bool Stack::isFull() const {
+    return (length == MAX_ITEMS);
+}
+
+void Stack::push(ItemType item) {
+    if (!isFull()) {
+        structure[length] = item;
+        length += 1;
+    } else {
+        throw "Stack is already full!";
+    }
+}
+
+ItemType Stack::pop() {
+    if (!isEmpty()) {
+        ItemType aux = structure[length - 1];
+        length -= 1;
+        return aux;
+    } else {
+        throw "Stack is empty!";
+    }
+}
+
+void Stack::print() const {
+    cout << "Pilha = ";
+    for (int i = 0; i < length; i += 1) {
+        cout << structure[i];
+    }
+    cout << endl;
+}
